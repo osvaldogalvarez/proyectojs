6144cc66ba3172e87eb1232fbe595c3fbb12e378
osvaldoalvarez <osvaldo_alvarez@live.com.ar>
2022-05-26 22:59:27 -0300

    cambios en js y en html

--- shopjs.js
+++ shopjs.js
@@ -1,55 +1,3 @@
-/*let titulo = document.getElementById("shopis");
-titulo.innerText = "GALERIA SHOP";
-
-let compras = document.getElementById("produc");
-console.log(compras);
-
-let fotis = document.getElementsByClassName("foti");
-console.log(fotis);
-
-let buton = document.getElementById("btnPrincipal");
-buton.onclick = () => {
-    console.log("click 2");
-}
-buton.onmousemove = () => {
-    console.log("mouse Move");
-}
-buton.onmouseover = () => {
-    console.log("mouse Over");
-}
-*/
- /*const Clickbutton = document.querySelectorAll('.button');
-const tbody = document.querySelector('.tbody');
-
-let carrito = []
-
-Clickbutton.forEach(btn => {
-  btn.addEventListener('click', addToCarritoItem);
-})
-function addToCarritoItem(e){
-    const button = e.target
-    const item = button.closest('.card');
-    const itemTitle = item.querySelector('.card-title').textContent;
-    const itemPrice = item.querySelector('.precio').textContent;
-    const itemImg = item.querySelector('.card-img-top').src;
-    
-    const newItem = {
-        title: itemTitle,
-        precio: itemPrice,
-        img: itemImg,
-        cantidad: 1
-      }
-    
-      addItemCarrito(newItem);
-    }
-
-    function addItemCarrito(newItem){
-
-        carrito.push(newItem);
-       // console.log(carrito);
-       
-    }
-*/
 
 const Clickbutton = document.querySelectorAll('.button')
 const tbody = document.querySelector('.tbody')
@@ -190,4 +138,19 @@ window.onload = function(){
     carrito = storage;
     renderCarrito()*/
   }
+  let url = 'https://jsonplaceholder.typicode.com/users/';
+        fetch(url)
+            .then( response => response.json() )
+            .then( data => mostrarData(data) )
+            .catch( error => console.log(error) )
+
+        const mostrarData = (data) => {
+            console.log(data)
+            let body = ""
+            for (var i = 0; i < data.length; i++) {      
+               body+=`<tr><td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].email}</td></tr>`
+            }
+            document.getElementById('data').innerHTML = body
+            console.log(body)
+        }
   
